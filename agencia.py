d5affb97eff823e31921c57dbb237db1186d5dc6
Denner <dennerms35@gmail.com>
2024-07-23 11:17:27 -0300

    criação dos metodos paypal para agencia virtual

--- agencia.py
+++ agencia.py
@@ -1,53 +1,60 @@
 from random import randint
 
+
 class Agencia:
 
-    def __init__(self,telefone,cnpj,numero):
+    def __init__(self, telefone, cnpj, numero):
         self.telefone = telefone
         self.cnpj = cnpj
         self.numero = numero
         self.clientes = []
         self.caixa = 0
         self.emprestimos = []
-        
+
     def verificar_caixa(self):
         if self.caixa < 1000000:
             print("Caixa abaixo do nível recomendado. Caixa atual: {}".format(self.caixa))
         else:
             print(f'O Valor do caixa esta ok. Caixa atual {self.caixa}')
 
-    def emprestar_dinheiro(self,valor,cpf,juros):
-        if self.caixa >valor:
-            self.emprestimos.append((valor,cpf,juros))
+    def emprestar_dinheiro(self, valor, cpf, juros):
+        if self.caixa > valor:
+            self.emprestimos.append((valor, cpf, juros))
             print('Emprestimo efetuado')
         else:
             print("Caixa insuficiente para efetuar o empréstimo solicitado.")
-    
-    def adicionar_clientes(self,nome,cpf,patrimonio):
-        self.clientes.append((nome,cpf,patrimonio))
+
+    def adicionar_clientes(self, nome, cpf, patrimonio):
+        self.clientes.append((nome, cpf, patrimonio))
 
 
 class AgenciaVirtual(Agencia):
-    def __init__(self,site,telefone,cnpj,numero):
+    def __init__(self, site, telefone, cnpj, numero):
         self.site = site
-        super().__init__(telefone,cnpj,numero)
+        super().__init__(telefone, cnpj, numero)
+        self.caixa = 1000000
+        self.caixa_paypal = 0
+
+    def depositar_paypal(self, valor):
+        self.caixa -= valor
+        self.caixa_paypal += valor
+    
+    def sacar_paypal(self,valor):
+        self.caixa += valor
+        self.caixa_paypal -= valor
+
 
 class AgenciaComum(Agencia):
-    def __init__(self,telefone,cnpj):
-        super().__init__(telefone,cnpj,randint(1001,9999))
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, randint(1001, 9999))
         self.caixa = 1000000
 
+
 class AgenciaPremium(Agencia):
-    def __init__(self,telefone,cnpj):
-        super().__init__(telefone,cnpj,randint(1001,999))
+    def __init__(self, telefone, cnpj):
+        super().__init__(telefone, cnpj, randint(1001, 999))
         self.caixa = 50000000
 
 
-
-
-
-
-
-
-agencia_virtual = AgenciaVirtual('www.',645626,645692,2458)
+agencia_virtual = AgenciaVirtual('www.', 645626, 645692, 2458)
 print(agencia_virtual.__dict__)
