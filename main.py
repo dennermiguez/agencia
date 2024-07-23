eac07bc806f5a2fdd770b48edb202325592b27bf
Denner <dennerms35@gmail.com>
2024-07-23 10:44:37 -0300

    criação do main

---
+++ main.py
@@ -0,0 +1,7 @@
+from sistema_conta_corrente import Conta_corrente, CartaoCredito
+
+#cria uma nova instancia de conta corrente
+conta_denner = Conta_corrente("Denner",'100.555.668-15',1234,34062)
+
+#cria uma nova instancia de cartao de credito
+cartao_denner = CartaoCredito('Denner',conta_denner)
